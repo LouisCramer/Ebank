9aa89b6ed7d9ab2902c85b4471689c166bff9288
lcramerTNDM <136857851+lcramerTNDM@users.noreply.github.com>
2024-02-21 15:04:38 -0800

    Creating shopping cart with transaction wrappers

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,55 @@
 from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponse
 from django.db.models.aggregates import Count, Max, Min, Average, Sum
 from django.db.models import Q, F, Values, Func, ExpressionWrapper
-from django.db.models.functions import Concat
+from django.db.models.functions import Concat, transaction
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product, Customer, OrderItem
+from tags.models import TaggedItem
 
 
 def say_hello(request):
+    
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 1
+        order.save()
+        
+        item = OrderItem()
+        item.order = order
+        item.product_id = 1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
+    
+    # Creating item in cart
+    cart = Cart
+    cart.save()
+    
+    Mustard = CartItem()
+    Mustard.cart = cart
+    Mustard.product_id = 1
+    Mustard.quantity = 1
+    Mustard.save()
+    
+    # updating item in cart
+    Mustard = CartItem.objects.get(pk=1)
+    Mustard.quantity = 2
+    Mustard.save()
+    
+    # Deleting item from cart
+    cart = Cart(pk=1)
+    cart.delete()
+    
+    collection = Collection.get(pk=11)
+    collection.delete()
+    
+    Collection.objects.filter(id_gt=5).delete()
+    
+    
+    # Collection.objects.filter(pk=11).update(featured_product=None)
+    
     discounted_price = ExpressionWrapper(
         F('unit_price') * 0.8, output_field=DecimalField())
     
@@ -45,7 +87,7 @@ def say_hello(request):
     queryset = Product.objects.prefetch_related('promotions').select_related('collection')
    
     
-    return render(request, 'hello.html', {'name': 'Louy', 'result': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Louy', 'result', 'tags': list(queryset)})
     # slice element for first 5 operators = 'query_set[0:5]'
     # for product in query_set:
         # print(product)
