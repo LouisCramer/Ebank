937db962bd72cd407ba1e7dec8a3da07cbc5a5dd
lcramerTNDM <136857851+lcramerTNDM@users.noreply.github.com>
2024-03-12 13:56:13 -0700

    Updating Syntax for new version of VS Code +1

--- store/serializer.py
+++ store/serializer.py
@@ -50,13 +50,10 @@ class CartSerializer(serializers.ModelSerializer):
     total_price = serializers.SerializerMethodField()
     
     def get_total_price(self, cart):
-        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
+        return sum([item.quantity * item.product.unit_price 
+                    for item in cart.items.all()])
         
     class Meta: 
         model = Cart
         fields = ['id', 'items', 'total_price']
-    
-    
-    
-    
-    
+
