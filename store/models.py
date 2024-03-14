64e868ed466cf862ebbd80b6b312bae64f85c0b0
lcramerTNDM <136857851+lcramerTNDM@users.noreply.github.com>
2024-03-14 13:44:58 -0700

    updates 2

--- store/models.py
+++ store/models.py
@@ -58,6 +58,7 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    user = models.OneToOneField()
     
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
