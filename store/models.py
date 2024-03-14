88de4f8fb5ff12f87d04b16841cecb7279b8a105
lcramerTNDM <136857851+lcramerTNDM@users.noreply.github.com>
2024-03-14 15:57:07 -0700

    Settings module core

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
@@ -51,17 +52,14 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold'),
     ]
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-    user = models.OneToOneField()
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.USER.first_name} {self.USER.last_name}'
 
     class Meta:
         ordering = ['first_name', 'last_name']
