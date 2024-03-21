327aa89e7aa44106cf355bdf2ab1d425d8dccb4c
lcramerTNDM <136857851+lcramerTNDM@users.noreply.github.com>
2024-03-21 10:11:12 -0700

    Completed Orders API with Section 2

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
-    
+
     def __str__(self) -> str:
         return self.title
 
@@ -27,18 +27,18 @@ class Product(models.Model):
     slug = models.SlugField()
     description = models.TextField(null=False, blank=True)
     unit_price = models.DecimalField(
-        max_digits=6, 
+        max_digits=6,
         decimal_places=2,
         validators=[MinValueValidator(1)])
     inventory = models.IntegerField(validators=[MinValueValidator(1)])
     last_update = models.DateTimeField(auto_now=True)
-    collection = models.ForeignKey(Collection, on_delete=models.PROTECT, 
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT,
                                    related_name='products')
     promotions = models.ManyToManyField(Promotion, blank=True)
- 
+
     def __str__(self) -> str:
         return self.title
- 
+
     class Meta:
         ordering = ['title']
 
@@ -59,10 +59,10 @@ class Customer(models.Model):
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
- 
+
     def __str__(self):
         return f'{self.USER.first_name} {self.USER.last_name}'
- 
+
     @admin.display(ordering='user__first_name')
     def first_name(self):
         return self.user.first_name
@@ -102,8 +102,8 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT, 
-                                related_name='orderitems')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT,
+                                related_name='order_items')
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
--- store/views.py
+++ store/views.py
@@ -15,7 +15,7 @@ from .pagination import DefaultPagination
 from .serializer import ProductSerializer, CollectionSerializer
 from .serializer import UpdateCartItemSerializer, AddCartItemSerializer
 from .serializer import CartItemSerializer, CustomerSerializer, CartSerializer
-from .serializer import OrderSerializer, ReviewSerializer
+from .serializer import OrderSerializer, ReviewSerializer, CreateOrderSerializer, UpdateOrderSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -27,53 +27,54 @@ class ProductViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
-    
+
     def get_queryset(self):
         queryset = Product.objects.all()
         collection_id = self.request.query_params.get('collection_id')
         if collection_id is not None:
             queryset = queryset.filter(collection_id=collection_id)
-        
+
         return queryset
-    
+
     def get_serializer_context(self):
         return {'request': self.request}
 
-    def destroy(self, request):
+    def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
-            return Response({'error': 'Product cannot be deleted as it is associated with an order'}),
-        
+            return Response(
+                {'error': 'Product cannot be deleted as it is associated with an order'}),
+
         return super().destroy(request, *args, **kwargs)
 
-   
+
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(
         products_count=Count('products')).all()
     serializer_class = CollectionSerializer
-    
+
     def delete(self, request, pk):
         collection = get_object_or_404(Collection, pk=pk)
         if collection.products.count() > 0:
             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'})
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+
 
 class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
-    
+
     def get_queryset(self):
         queryset = Product.objects.all()
         collection_id = self.request.query_params['collection_id']
         if collection_id is not None:
             queryset = queryset.filter(collection_id=collection_id)
-        
-    def get_serializer_context(self):
+
+    def get_serializer_context(self, **kwargs):
         return Review.objects.filter(product_id=sel.kwargs['product_pk'])
-    
+
     # def get_serializer_context(self):
         # return {'product_id': self.kwargs['product_pk']}
-    
+
 
 class CartViewSet(CreateModelMixin,
                   RetrieveModelMixin,
@@ -81,45 +82,45 @@ class CartViewSet(CreateModelMixin,
                   DestroyModelMixin):
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
-    
+
 
 class CartItemViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
-    
+
     def get_serializer(self):
         if self.request.method == 'POST':
             return AddCartItemSerializer
         elif self.request.method == 'PATCH':
             return UpdateCartItemSerializer
         return CartItemSerializer
-       
+
     def get_serializer_context(self):
         return {'cart_id': self.kwargs['cart_pk']}
-       
+
     def get_queryset(self):
         return CartItem.objects \
                .filter(cart_id=self.kwargs['cart_pk']) \
                .select_related('product')
-    
+
 
 class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [IsAdminUser]
-    
+
     @action(detail=True, permission_classes=[ViewCustomerHistoryPermission])
     def history(self, request, pk):
         return Response('ok')
-    
+
     def get_permissions(self):
         if self.request.method == 'GET':
             return [AllowAny()]
         return [IsAuthenticated]
-    
+
     @action(detail=False, methods=['GET', 'PUT'],
             permission_classes=[IsAuthenticated])
     def me(self, request):
-        (customer, created) = Customer.objects.get_or_create(
+        customer = Customer.objects.get(
             user_id=request.user.id)
         if request.method == 'GET':
             serializer = CustomerSerializer(customer)
@@ -131,8 +132,35 @@ class CustomerViewSet(ModelViewSet):
 
 
 class OrderViewSet(ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    
-    def orders(self, request):
-        return 
+    http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(
+            data=request.data,
+            context={'user_id': self.request.user.id})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateOrderSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateOrderSerializer
+        return OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_staff:
+            return Order.objects.all()
+
+        customer_id = Customer.objects.only(
+            'id').get(user_id=user.id)
+        return Order.objects.filter(customer_id=customer_id)
