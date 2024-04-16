c29d1dd9beb5f27a276327d7316eaaa8e0b878a1
lcramer <lcramer@tandemdiabetes.com>
2024-04-16 14:38:10 -0700

    Email sender

--- storefront3/store-main/playground/views.py
+++ storefront3/store-main/playground/views.py
@@ -1,5 +1,11 @@
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
+    try:
+        mail_admins('subject', 'message', html_message='message')
+        send_mail('subject', 'message', 'lcramer@tandemdiabetes.com', [''])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
